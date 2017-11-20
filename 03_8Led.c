4fb6548800d796a63f9a9e2b99e8053808a9c5ad
Iaroslav Karandashev <yarix7@gmail.com>
2017-11-20 03:33:52 +0000

    First try of lesson 3.wq

---
+++ 03_8Led.c
@@ -0,0 +1,24 @@
+#include <wiringPi.h>
+#include <stdio.h>
+
+int main(void){
+    wiringPiSetup();
+
+    int leds[4] = {0,1,2,3};
+
+    int i;
+    for (i = 0; i < 3; i++) {
+	pinMode(leds[i], OUTPUT);
+    }
+
+    while(1) {
+        for (i = 0; i < 3; i++) {
+            digitalWrite(leds[i], LOW);
+	    delay(500);
+	}
+	for (i = 0; i < 3; i++) {
+	    digitalWrite(leds[i], HIGH);	
+	}
+    }
+    return 0;
+}
