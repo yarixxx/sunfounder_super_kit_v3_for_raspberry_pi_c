1d3979065cc22411e737dd77b0ce5b7822fb5b4b
Iaroslav Karandashev <yarix7@gmail.com>
2017-12-11 01:43:02 +0000

    Fix lcd

--- 13_LCD1602.c
+++ 13_LCD1602.c
@@ -2,24 +2,33 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 #include <lcd.h>
+#include "t_extension.h"
 
-const unsigned char Buf[] = "---SUNFOUNDER---";
-const unsigned char myBuf[] = "  sunfounder.com";
+#define ROWS 2
+#define COLUMNS 16
+#define BITS 4
+#define RS 2
+#define STRB 3
 
 int main(void) {
 	int fd;
 	wiringPiSetup();
 
-	fd = lcdInit(2,16,4, 2,3, 6,5,4,1,0,0,0,0); //see /usr/local/include/lcd.h
+     // There is something strange here, D0-D3 shouldn't be used, D4-D7 should be.
+     // 
+     // fd = lcdInit(2,    16,      4,    2,  3,    6, 5, 4, 1, 0, 0, 0, 0); //see /usr/local/include/lcd.h
+	fd = lcdInit(ROWS, COLUMNS, BITS, RS, STRB, 6, 5, 4, 1, 0, 0, 0, 0);	
 
 	int i;
 	i = 0;
-	while(1){
+	char scounter[15];
+	while(1){		
+		sprintf(scounter, "N: %d", i);
 		lcdClear(fd);
 		lcdPosition(fd, 0, 0);
 		lcdPuts(fd, "Hello!");
 		lcdPosition(fd, 0, 1);
-		lcdPuts(fd, "World");
+		lcdPuts(fd, scounter);
 		delay(1000);
 		i++;
 	}
